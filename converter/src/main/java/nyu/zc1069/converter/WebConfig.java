4073702d1cc8a1d2a02766bae00010335ddde539
James Chen <zc1069@nyu.edu>
2020-07-21 09:27:27 -0400

    multiple cors

---
+++ converter/src/main/java/nyu/zc1069/converter/WebConfig.java
@@ -0,0 +1,16 @@
+package nyu.zc1069.converter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://127.0.0.1:8081", "http://localhost:8081");
+    }
+}
--- converter/src/main/java/nyu/zc1069/converter/api/YouTubeController.java
+++ converter/src/main/java/nyu/zc1069/converter/api/YouTubeController.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://localhost:8081")
 //@CrossOrigin(origins = "http://127.0.0.1:8081")
 @RequestMapping("api/v1/youtube")
 @RestController
