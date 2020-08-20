3241f097596a6c64cb21600fa8a8233f0493abc0
James Chen <zc1069@nyu.edu>
2020-08-20 08:54:54 -0400

    added vuetify

--- converter-gui/src/views/Home.vue
+++ converter-gui/src/views/Home.vue
@@ -1,7 +1,40 @@
 <template>
   <div class="home">
       <router-link to="/step1">Click here to start</router-link>
+
+  <v-card
+    class="mx-auto"
+    max-width="400"
+    tile
+  >
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Single-line item</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title>Three-line item</v-list-item-title>
+        <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          consectetur adipiscing elit.
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
   </div>
+
+
 </template>
 
 <script>
--- converter-gui/vue.config.js
+++ converter-gui/vue.config.js
@@ -1,4 +1,6 @@
-// vue.config.js
 module.exports = {
-    publicPath: "/converter/"
+  "publicPath": "/converter/",
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
