c383b9d205bbf03dc062e077981bbc4a8673b052
James Chen <zc1069@nyu.edu>
2021-12-19 15:14:58 -0500

    Update converter-gui vue.config.js to output generated content to target/dist

--- converter-gui/vue.config.js
+++ converter-gui/vue.config.js
@@ -1,4 +1,7 @@
 // vue.config.js
-module.exports = {
-    publicPath: "/converter/"
+module.exports = {  
+    // Change build paths to make them Maven compatible
+    // see https://cli.vuejs.org/config/
+    outputDir: 'target/dist',
+    assetsDir: 'static'
 }
